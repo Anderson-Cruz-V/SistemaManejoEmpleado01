f93e4e7813f1162e805320e9ee792be3678156f9
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2025-07-16 21:42:35 -0400

    Conexión a base de datos y creación de modelos y DbContext completados.

---
+++ SistemaManejoEmpleados/SistemaManejoEmpleados/Models/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaManejoEmpleados.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public ICollection<Cargo> Cargos { get; set; }
+    }
+}
---
+++ SistemaManejoEmpleados/SistemaManejoEmpleados/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaManejoEmpleados.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Cedula { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public int CargoId { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+}
